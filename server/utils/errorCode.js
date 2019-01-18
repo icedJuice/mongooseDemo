9ae3858d16f014097f6c61977a3bbd8041b4e178
hanguozhi <hanguozhi@bestcode.com>
2019-01-18 15:57:04 +0800

    01

--- server/utils/errorCode.js
+++ server/utils/errorCode.js
@@ -1,14 +1,3 @@
-// module.exports = {
-//     //全局
-//     'sever error.': 500,
-
-//     // 注册登录相关
-//     'ok': 200,
-//     'invalid username or password.': 301,
-//     'error incorrect username or password.': 302,
-//     'user name already exists.': 305
-
-// }
 module.exports = {
     //全局
     500: 'sever error.',
